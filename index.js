415337059514c559d333326ef541e9d4a043f443
Cathy Lill <cathygail.lill@gmail.com>
2014-08-28 19:42:49 +1000

    node all the things!

---
+++ index.js
@@ -0,0 +1,43 @@
+var center = require('turf-center');
+var point = require('turf-point');
+var linestring = require('turf-linestring');
+
+//auto detect location
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Your browser doesn't support your location")
+    }
+}
+
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+
+    var markerlat = -37.8333;
+    var markerlon = 145.0000;
+
+    // create a map in the "map" div, set the view to a given place and zoom
+    var map = L.map('map').setView([lat,lon], 13);
+
+    // add an OpenStreetMap tile layer
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    //map markers
+    var marker = L.marker([lat,lon]).addTo(map)
+        .bindPopup("<b>To there!</b><br />Destination.").openPopup();
+
+    function onMapClick(e) {
+        L.popup()
+            .setLatLng(e.latlng)
+            .setContent("Start my journey at " + e.latlng.toString())
+            .openOn(map);
+    }
+
+    map.on('click', onMapClick);
+}
+
+getLocation();
